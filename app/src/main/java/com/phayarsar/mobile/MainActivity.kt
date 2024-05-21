0f45509c39ce97613a929410984fffea330e7ac7
winechit-dev <winechitpaing.dev@gmail.com>
2024-05-21 21:11:27 +0630

    chore: theme name changes

--- app/src/main/java/com/phayarsar/mobile/MainActivity.kt
+++ app/src/main/java/com/phayarsar/mobile/MainActivity.kt
@@ -11,14 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.phayarsar.design_system.theme.PhayarSarTheme
+import com.phayarsar.design_system.theme.PysTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            PhayarSarTheme {
+            PysTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
@@ -41,7 +41,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    PhayarSarTheme {
+    PysTheme {
         Greeting("Android")
     }
 }
