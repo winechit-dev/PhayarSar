686a2bfb8b4e75e01ad38ea92efe5509af019f1f
winechit-dev <winechitpaing.dev@gmail.com>
2024-05-23 00:39:54 +0630

    chore: create add worship plan card ui

--- feature/home/src/main/java/com/phyarsar/home/components/AddWorshipPlanCard.kt
+++ feature/home/src/main/java/com/phyarsar/home/components/AddWorshipPlanCard.kt
@@ -1,26 +1,53 @@
 package com.phyarsar.home.components
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.phayarsar.design_system.components.PysCard
+import com.phayarsar.design_system.components.PysOutlinedButton
 import com.phayarsar.design_system.theme.PysPreview
 import com.phayarsar.design_system.theme.ThemePreviews
+import com.phayarsar.localization.Vocabulary
 import com.phyarsar.home.R
 
 @Composable
-fun AddWorshipPlanCard() {
+fun AddWorshipPlanCard(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = {}
+) {
+    val localization = Vocabulary.localization
     PysCard(
-        color = MaterialTheme.colorScheme.primaryContainer.copy(alpha = 0.4f),
+        modifier = modifier,
+        color = MaterialTheme.colorScheme.secondaryContainer,
     ) {
-        Row{
+        Row(
+            modifier = Modifier.padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
             Icon(
                 painter = painterResource(id = R.drawable.ic_playlist_add_24),
                 contentDescription = "Add Worship Plan",
+                modifier = Modifier.size(32.dp)
             )
-
+            Text(
+                text = localization.worship_plan_helps_you_pray,
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier.weight(1f)
+            )
+            PysOutlinedButton(
+                onClick = onClick,
+                text = localization.add_new
+            )
         }
     }
 }
