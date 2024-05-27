d3c0a1d3e4dec3aa556cdf34e2569d54c036b1b6
lwin lwin cho <lwinlwincho135324@gmail.com>
2024-05-27 11:25:14 +0630

    remove: data from DummyData to PreviewData

--- feature/home/src/main/java/com/phyarsar/home/PreviewData.kt
+++ feature/home/src/main/java/com/phyarsar/home/PreviewData.kt
@@ -1,4 +1,44 @@
 package com.phyarsar.home
 
+import com.phyarsar.home.model.LanguageModel
+import com.phyarsar.home.model.PrayerModel
+
 object PreviewData {
+
+    val languageList = listOf(
+        LanguageModel(
+            languageName = "English",
+            languageNotation = "(English)",
+            image = R.drawable.ic_uk_flag,
+            isChecked = false
+        ),
+        LanguageModel(
+            languageName = "Myanmar",
+            languageNotation = "(မြန်မာစာ)",
+            image = R.drawable.ic_myanmar_flag,
+            isChecked = true
+        )
+    )
+
+    val previewPrayerList = listOf(
+        "သြကာသ ကန်တော့ချိုး",
+        "စိန်ရောင်ခြည် ဘုရားပင့်",
+        "နတ်ပင့်",
+        "သြကာသ ကန်တော့ချိုး",
+        "စိန်ရောင်ခြည် ဘုရားပင့်",
+        "နတ်ပင့်"
+    )
+
+    val previewOtherPrayerList = listOf(
+        PrayerModel("1", "သမ္ဗုဒ္ဓေ ဂါထာတော်ကြီး"),
+        PrayerModel("2", "ရှင်သီဝလိ ဂါထာတော်"),
+        PrayerModel("3", "ဓမ္မစကြာ တရားတော်"),
+        PrayerModel("4", "သမ္ဗုဒ္ဓေ ဂါထာတော်ကြီး"),
+        PrayerModel("5", "ရှင်သီဝလိ ဂါထာတော်"),
+        PrayerModel("6", "ဓမ္မစကြာ တရားတော်"),
+        PrayerModel("7", "သမ္ဗုဒ္ဓေ ဂါထာတော်ကြီး"),
+        PrayerModel("8", "ရှင်သီဝလိ ဂါထာတော်"),
+        PrayerModel("9", "ဓမ္မစကြာ တရားတော်"),
+        PrayerModel("10", "သမ္ဗုဒ္ဓေ ဂါထာတော်ကြီး")
+    )
 }
