7842c1fb93b69f52226f196a0b642d3a501c6ed1
lwin lwin cho <lwinlwincho135324@gmail.com>
2024-05-28 17:14:08 +0630

    add: kotlin serialization plugins to build.gradle (project level)

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,4 +6,5 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
 }
