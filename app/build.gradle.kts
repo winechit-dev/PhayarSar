9c0a55ff47d0f00b6990c31c9c4e0b9039b51017
winechit-dev <winechitpaing.dev@gmail.com>
2024-08-11 13:31:01 +0630

    refactor: remove core:data and implement core:domain

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,7 @@ android {
 }
 
 dependencies {
-    implementation(project(":core:data"))
+    implementation(project(":core:domain"))
     implementation(project(":core:design-system"))
     implementation(project(":core:localization"))
     implementation(project(":feature:home"))
