35b8021e844901a2fd1c11ef9db9a0ac72640760
winechit-dev <winechitpaing.dev@gmail.com>
2024-08-11 13:11:47 +0630

    chore: android data store library implementation

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -16,6 +16,10 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    
+    implementation(libs.androidx.dataStore.pref)
+    implementation(libs.androidx.dataStore.core)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
