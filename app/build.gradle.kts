1ae47554accc688af3f190518d0e644ec5fac02f
winechit-dev <winechitpaing.dev@gmail.com>
2025-02-06 12:15:23 +0700

    add: install splash screen library and theme xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,4 +60,6 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation(libs.androidx.core.splashscreen)
 }
